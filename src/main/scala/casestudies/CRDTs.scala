41e2e57c575c507c11b5a81ce00de40f26c24cc2
Jeremie Jost <jeremie.jost@zalando.ie>
2019-03-24 16:17:56 +0100

    CRDTs: generic KVStore type class based impl

--- src/main/scala/casestudies/CRDTs.scala
+++ src/main/scala/casestudies/CRDTs.scala
@@ -2,7 +2,8 @@ package casestudies
 
 import cats.syntax.semigroup._
 import cats.instances.map._
-import sun.misc.GC
+import cats.instances.list._
+import cats.syntax.foldable._
 
 object CRDTs extends App {
   import cats.Monoid
@@ -51,6 +52,52 @@ object CRDTs extends App {
     override def total(map: Map[K, V])(implicit m: Monoid[V]): V =
       m.combineAll(map.values)
   }
+
+  trait KeyValueStore[F[_,_]] {
+    def put[K, V](f: F[K, V])(k: K, v: V): F[K, V]
+    def get[K, V](f: F[K, V])(k: K): Option[V]
+    def getOrElse[K, V](f: F[K, V])(k: K, default: V): V =
+      get(f)(k).getOrElse(default)
+    def values[K, V](f: F[K, V]): List[V]
+  }
+
+  object KeyValueStore {
+    implicit def mapKVInstance[K, V] : KeyValueStore[Map] = new KeyValueStore[Map] {
+      override def put[K, V](f: Map[K, V])(k: K, v: V): Map[K, V] = f.updated(k, v)
+
+      override def get[K, V](f: Map[K, V])(k: K): Option[V] = f.get(k)
+
+      override def values[K, V](f: Map[K, V]): List[V] = f.values.toList
+    }
+  }
+
+  implicit class KvsOps[F[_,_], K, V](f: F[K, V]) {
+    def put(key: K, value: V)
+           (implicit kvs: KeyValueStore[F]): F[K, V] =
+      kvs.put(f)(key, value)
+    def get(key: K)(implicit kvs: KeyValueStore[F]): Option[V] =
+      kvs.get(f)(key)
+    def getOrElse(key: K, default: V)
+                 (implicit kvs: KeyValueStore[F]): V =
+      kvs.getOrElse(f)(key, default)
+    def values(implicit kvs: KeyValueStore[F]): List[V] =
+      kvs.values(f)
+  }
+
+  implicit def gcounterInstance[F[_,_], K, V]
+  (implicit kvs: KeyValueStore[F], km: Monoid[F[K, V]]) =
+    new GCounter[F, K, V] {
+      def increment(f: F[K, V])(key: K, value: V)
+                   (implicit m: Monoid[V]): F[K, V] = {
+        val total = f.getOrElse(key, m.empty) |+| value
+        f.put(key, total)
+      }
+      def merge(f1: F[K, V], f2: F[K, V])
+               (implicit b: BoundedSemiLattice[V]): F[K, V] =
+        f1 |+| f2
+      def total(f: F[K, V])(implicit m: Monoid[V]): V =
+        f.values.combineAll
+    }
 
   import cats.instances.int._ // for Monoid
 
